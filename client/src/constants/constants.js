449032f92240abf48635dff7fc5c615603548050
Sarmad Masood <sarmad1masaud@gmail.com>
2025-08-28 22:16:49 +0100

    server url change

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,1 +1,14 @@
-export const serverURL = "https://tiktokwebapp-e6c8dvhca9f3hrfq.uksouth-01.azurewebsites.net"
+//export const serverURL = "http://localhost:5000/"
+
+// client/src/constants/constants.js
+let serverURL;
+
+if (process.env.NODE_ENV === 'production') {
+    // Production: Use Azure App Service URL
+    serverURL = "tiktokwebapp-e6c8dvhca9f3hrfq.uksouth-01.azurewebsites.net";
+} else {
+    // Development: Use local server
+    serverURL = "http://localhost:5000/";
+}
+
+export { serverURL };
