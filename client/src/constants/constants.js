c7f0a897723acd5d66c0248d8319bdaf30b138d0
Sarmad Masood <sarmad1masaud@gmail.com>
2025-08-28 23:47:19 +0100

    changed to win

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -5,7 +5,7 @@ let serverURL;
 
 if (process.env.NODE_ENV === 'production') {
     // Production: Use Azure App Service URL
-    serverURL = "tiktokwebapp-e6c8dvhca9f3hrfq.uksouth-01.azurewebsites.net";
+    serverURL = "tiktokwebwin-bsb7c6fwdfbudact.uksouth-01.azurewebsites.net";
 } else {
     // Development: Use local server
     serverURL = "http://localhost:5000/";
