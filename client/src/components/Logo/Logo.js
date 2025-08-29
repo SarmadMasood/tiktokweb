0b918cd6b24b2e8bbaad494c41ee0d427c6d2d6b
Sarmad Masood <sarmad1masaud@gmail.com>
2025-08-29 07:18:31 +0100

    CW2 Presentation

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Logo = () => {
-  return (
-    <div className="text-2xl font-bold pl-0 pr-10 lg:px-10">
-      WeTube
-    </div>
-  )
+    return ( <
+        div className = "text-2xl font-bold pl-0 pr-10 lg:px-10" >
+        Tiktok Web Sarmad <
+        /div>
+    )
 }
 
 export default Logo
